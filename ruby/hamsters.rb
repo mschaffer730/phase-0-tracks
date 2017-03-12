28e2f125d47701f6a1bc420ba6c71ddc62c126f6
Mike Schaffer <mike.schaffer@hotmail.com>
2017-03-12 15:56:50 -0500

    completed hamsters pairing tasks and code

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,15 +1,28 @@
 puts "Hamster's name?"
 name = gets.chomp
+
 puts "volume level 1-10?"
-volume = gets.chomp
+volume = gets.to_i
+
+
 puts "fur color?"
 color = gets.chomp
+
+
 puts "good candidate for adoption?"
 adoption = gets.chomp
+
+
 puts "estimated age?" 
 age = gets.strip
 if age.empty?
   age = nil
 else
-  age = number.to_i
+  age = age.to_i
 end
+
+puts "Hamster Name:#{name}"
+puts "Volume Level:#{volume}"
+puts "Fur Color:#{color}"
+puts "Good Candidate:#{adoption}"
+puts "Age:#{age}"
